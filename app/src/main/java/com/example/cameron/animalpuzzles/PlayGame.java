c834f95e319c16967b72575a0c33e8d348d0d5af
Harlin Allen <jacksta67@xtra.co.nz>
2015-05-20 15:02:52 +1200

    added a second text button

---
+++ app/src/main/java/com/example/cameron/animalpuzzles/PlayGame.java
@@ -0,0 +1,56 @@
+package com.example.cameron.animalpuzzles;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+
+
+
+public class PlayGame extends ActionBarActivity {
+    ImageView testImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+        ImageView testImage = (ImageView) findViewById(R.id.imageView);
+
+        testImage.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PlayGame.this, SelectPuzzle.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_play_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
