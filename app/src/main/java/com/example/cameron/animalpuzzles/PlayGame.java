6580ac9e6c158c8ebee71825f56101bb96d08776
Harlin Allen <jacksta67@xtra.co.nz>
2015-05-21 09:01:34 +1200

    First iteration of drag and drop

--- app/src/main/java/com/example/cameron/animalpuzzles/PlayGame.java
+++ app/src/main/java/com/example/cameron/animalpuzzles/PlayGame.java
@@ -1,8 +1,15 @@
 package com.example.cameron.animalpuzzles;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,16 +25,85 @@ public class PlayGame extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
-        ImageView testImage = (ImageView) findViewById(R.id.imageView);
 
-        testImage.setOnClickListener(new OnClickListener() {
+        //ImageView
+        ImageView testImage = (ImageView) findViewById(R.id.imageView);
+        testImage.setBackgroundColor(Color.rgb(100, 100, 50));
+
+
+        testImage.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(PlayGame.this, SelectPuzzle.class);
-                startActivity(i);
+            public boolean onLongClick(View v) {
+
+                //DragShadow dragShadow = new DragShadow(v);
+                ClipData clip = ClipData.newPlainText("", "");
+                v.startDrag(clip,  new View.DragShadowBuilder(v), v, 0);
+                return true;
+            }
+        });
+
+        ImageView testImage2 = (ImageView) findViewById(R.id.imageView2);
+
+        testImage2.setOnDragListener(new View.OnDragListener() {
+
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+
+                int dragEvent = event.getAction();
+
+                switch(dragEvent){
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.i("Drag Event", "Enetered Piece Area");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED  :
+                        Log.i("Drag Event", "Left Piece Area");
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        Log.i("Drag Event", "Piece Dropped");
+                        ImageView target = (ImageView) v;
+                        //returns the dragged information passed into dragged object
+                        ImageView dragged = (ImageView) event.getLocalState();
+                        target.setBackgroundColor(Color.BLUE);
+                        int image = R.drawable.ic_launcher;
+                        target.setImageResource(image);
+                        break;
+                }
+
+                return true;
             }
         });
     }
+    /*
+
+    private class DragShadow extends View.DragShadowBuilder{
+
+        //ColorDrawable greybox;
+
+
+        public DragShadow(View view) {
+            super(view);
+           // greybox = new ColorDrawable(Color.LTGRAY);
+        }
+
+        @Override
+        public void onDrawShadow(Canvas canvas) {
+            //greybox.draw(canvas);
+        }
+
+        @Override
+        public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+         //  View v = getView();
+          //  greybox.setBounds(0,0,48,48);
+           // shadowSize.set(48,48);
+            shadowTouchPoint.set(48/2,48/2);
+
+        }
+
+
+    }
+    */
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,7 +126,4 @@ public class PlayGame extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
 }
-
