0985ac5c8b0cd6d0345f5ee8b63795e1c888ab0d
Cameron <camsmithAUT@gmail.com>
2015-05-22 10:06:17 +1200

    Added onTouch Listeners, Drag and drop

--- app/src/main/java/com/example/cameron/animalpuzzles/SelectPuzzle.java
+++ app/src/main/java/com/example/cameron/animalpuzzles/SelectPuzzle.java
@@ -1,17 +1,31 @@
 package com.example.cameron.animalpuzzles;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SelectPuzzle extends ActionBarActivity {
+Button playbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_puzzle);
+        playbtn = (Button)findViewById(R.id.playbtn);
+
+        playbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SelectPuzzle.this,PlayGame.class);
+                startActivity(i);
+            }
+        });
     }
 
 
